1c5c613bc209da92901de7d4d780710b9fddf989
김기연 <kyean07@naver.com>
2019-07-24 20:38:22 +0900

    ㅇㅇㅇ

---
+++ Application/src/main/java/com/example/android/Perfect_fit/HomeActivity.java
@@ -0,0 +1,42 @@
+package com.example.android.Perfect_fit;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText("홈");
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText("내정보");
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText("설정");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
