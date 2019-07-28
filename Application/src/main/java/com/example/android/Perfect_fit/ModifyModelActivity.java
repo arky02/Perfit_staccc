64a8d757ff014e4215283c69825fe3cad10a949b
user <lmkn5342@gmail.com>
2019-07-28 11:39:19 +0900

    07/28 custom dialog 만듬

--- Application/src/main/java/com/example/android/Perfect_fit/ModifyModelActivity.java
+++ Application/src/main/java/com/example/android/Perfect_fit/ModifyModelActivity.java
@@ -1,10 +1,15 @@
 package com.example.android.Perfect_fit;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -12,6 +17,7 @@ import java.util.ArrayList;
 public class ModifyModelActivity extends AppCompatActivity {
 
     ImageView button_cancel;
+    Button btn_createModel;
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
 
@@ -21,6 +27,7 @@ public class ModifyModelActivity extends AppCompatActivity {
         setContentView(R.layout.activity_modify_model);
 
         button_cancel = findViewById(R.id.button_cancel);
+        btn_createModel = findViewById(R.id.btn_createModel);
         recyclerView = findViewById(R.id.recyclerView);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
@@ -46,6 +53,29 @@ public class ModifyModelActivity extends AppCompatActivity {
             }
         });
 
+        btn_createModel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateDialog();
+            }
+        });
 
     }
+
+    public void CreateDialog() {
+        final Dialog dialog = new Dialog(ModifyModelActivity.this);
+        if(dialog.getWindow() != null){
+            dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+        dialog.setContentView(R.layout.dialog_create_model);
+        dialog.show();
+
+    }
+
+//    public void Dialog() {
+//        final CustomDialog_CreateModel customDialog_createModel = new CustomDialog_CreateModel(ModifyModelActivity.this);
+//        customDialog_createModel.show();
+//    }
+
 }
